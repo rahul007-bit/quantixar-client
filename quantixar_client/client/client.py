3c586642351435598803f86903ee405b8c992be2
rahul007-bit <yadavrahul8141@yahoo.com>
2024-03-01 19:33:50 +0530

    not working

--- quantixar_client/client/client.py
+++ quantixar_client/client/client.py
@@ -1,9 +1,12 @@
 import requests
 import json
-from typing import List
+from typing import Dict, List, Union
 from .config import Config
 from .types import Payload
+import time
 
+
+BatchItem = Dict[str, Union[List[float], Payload]]
 
 class QuantixarClient:
     def __init__(self, host: str = "localhost", port: int = 8954, config: 'Config' = None):
@@ -21,6 +24,33 @@ class QuantixarClient:
     def get_api(self, endpoint: str) -> str:
         return f"http://{self.host}:{self.port}{endpoint}"
 
+    async def insert_batch(self, batch: List[List[BatchItem]]) -> None:
+        api = self.get_api("/vector")
+        body = []
+        print(type(batch))
+        for i, item in enumerate(batch):
+            # {points: [{vectors: [], payload: {}, id:1}]}
+            points = []
+            print(f"Processing batch {i}")
+            for j, point in enumerate(item):
+                id = round(time.time() * 1000)
+                points.append({
+                    "vectors": point["vectors"],
+                    "payload": point["payload"],
+                    "id": id
+                })
+        
+            body_point = json.dumps({"points": points})
+            body.append(body_point)
+            result = requests.post(api, data=body_point, headers={"Content-Type": "application/json"})
+            print(result.text)
+        
+        # save to json file
+        with open('data.json', 'w') as f:
+            json.dump(body, f)
+            f.close()
+        
+
     async def insert(self, value: List[float], payload: Payload) -> None:
         api = self.get_api("/vector")
         body = json.dumps({
@@ -30,8 +60,8 @@ class QuantixarClient:
         response = requests.post(api, data=body)
         return response.text
 
-    def query(self, value: List[float]) -> None:
+    def query(self, value: List[float], k: int = 10) -> None:
         api = self.get_api("/vector/search")
-        body = json.dumps({"vector": value,"k": 10})
+        body = json.dumps({"vector": value,"k": k})
         response = requests.post(api, data=body)
         return response.json()
--- quantixar_client/main.py
+++ quantixar_client/main.py
@@ -2,7 +2,7 @@ import json
 from sentence_transformers import SentenceTransformer
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
-from starlette.routing import Route
+from starlette.routing import Route, Mount
 import uvicorn
 from quantixar_client.utils.read_parquent import read_parquet,get_embeddings
 from quantixar_client.client.client import QuantixarClient
@@ -16,23 +16,35 @@ async def init(request):
     if request.app.state.initialized:
         return JSONResponse({"message": "Already initialized"})
     if len(payloads) == 0:
-        payloads = read_parquet("../top_1000_2.parquet")
-    for payload in payloads:
-        await client.insert(payload["vectors"], payload["payload"])
-        count += 1
-        if count % 100 == 0:
-            print(count)
-        if count == 1000:
-            break
+        payloads = read_parquet("./top_1000_2.parquet")
+    # for payload in payloads:
+    #     await client.insert(payload["vectors"], payload["payload"])
+    #     count += 1
+    #     if count % 100 == 0:
+    #         print(count)
+    #     if count == 1000:
+    #         break
+    
+    batch = [payloads[i:i+100] for i in range(0, len(payloads), 100)]
+    await client.insert_batch(batch)
+
+    request.app.state.initialized = True
     return JSONResponse({"message": "Insert successful"})
 
 async def query(request):
-    # Your query logic here
-    qurey = request.query_params['s']
-    embeddings = get_embeddings(model, qurey)
-    result = client.query(embeddings.tolist())
+    body = await request.json() # Parse the JSON body
+    limit = 10 if "limit" not in body else body["limit"]
+    queries = body.get("queries") # Use get method to avoid KeyError
+    if queries is None or queries.get("textQuery") is None:
+        return JSONResponse({"message": "No query provided"})
+    query = queries["textQuery"]
 
-    return JSONResponse(result)
+    embeddings = get_embeddings(model, query)
+    try:
+        result = client.query(embeddings.tolist(), limit)
+        return JSONResponse(result)
+    except Exception as e:
+        return JSONResponse({"message": "An error occurred"}, status_code=500)
 
 async def insert(request):
     body: bytes = await request.body()
@@ -44,13 +56,15 @@ async def index(request):
     return JSONResponse({"message": "Hello, world!"})
 
 app = Starlette(debug=True, routes=[
+    Mount('/api', routes=[
     Route("/", index),
     Route("/init", init, methods=["POST"]),
     Route("/query", query, methods=["POST"]),
+    ]),
 ])
 app.state.client = client
 app.state.initialized = False
 
 
 def main():
-    uvicorn.run("main:app", host="127.0.0.1", port=8000,reload=True,)
+    uvicorn.run(app, host="127.0.0.1", port=8000,)
